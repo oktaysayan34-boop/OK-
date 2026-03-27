592d8bfcee5bcb8e44442945a774761bf6a149be
oktaysayan34-boop <oktaysayan34@gmail.com>
2026-03-27 11:28:31 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+
+const CACHE_NAME = 'oktay-labcam-v1';
+const URLS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(()=>{});
+      return resp;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
